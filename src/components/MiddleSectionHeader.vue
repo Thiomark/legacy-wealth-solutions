a64309b72b23dbfd58f9aa2bd1d93e9ed1c9165f
itumeleng <thiotsoela@gmail.com>
2023-02-05 13:31:58 +0200

    create the middle section header

---
+++ src/components/MiddleSectionHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1 v-if="middle" class="grid-cols-3 w-fit mx-auto uppercase text-[1.25rem] grid items-center gap-4"><span class="text-center">{{title}} <span class="block -mt-3 text-[2.188rem]">{{subTitle}}</span></span> </h1>
+</template>
+
+<script>
+    export default {
+        name: 'MiddleSectionHeader',
+        props: {
+            middle: {
+                type: Boolean,
+                default: true
+            },
+            title: {
+                type: String,
+                required: true
+            },
+            subTitle: String
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    h1::before, h1::after{
+        content: "";
+        height: 2px;
+        display: block;
+    }
+</style>
